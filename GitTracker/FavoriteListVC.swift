9ea220ca26713e27223e0589c91c4aac5b897955
Theo2903 <theo@ribbi.ch>
2024-06-14 13:51:18 +0200

    Creating primary Navigation Controller

---
+++ GitTracker/FavoriteListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteListVC.swift
+//  GitTracker
+//
+//  Created by Ribbi Théo on 14.06.2024.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
