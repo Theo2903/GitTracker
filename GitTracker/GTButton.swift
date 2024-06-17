9688cbd13c3a7d26c1b2e7884d0a0263871689cc
Theo2903 <theo@ribbi.ch>
2024-06-17 21:45:53 +0200

    Create Button componenet

---
+++ GitTracker/GTButton.swift
@@ -0,0 +1,20 @@
+//
+//  GTButton.swift
+//  GitTracker
+//
+//  Created by Ribbi Théo on 17.06.2024.
+//
+
+import UIKit
+
+class GTButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
